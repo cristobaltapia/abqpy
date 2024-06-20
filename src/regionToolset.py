b2ab86c66ff4448dcc06e04df93950f4c58d7ebb
Cristóbal Tapia Camú <crtapia@gmail.com>
2024-06-21 00:09:31 +0200

    feat: add SurfaceType

--- src/regionToolset.py
+++ src/regionToolset.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 from abaqus.Region.Region import Region
 
+from abaqus.Region.Surface import Surface
+type SurfaceType = Surface
+
 __all__ = [
     "Region",
 ]
